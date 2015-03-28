34de119588f13261df0e95ebb6810206d96878c9
MKS-Student <mks.student@hackreactor.com>
2015-03-27 20:18:23 -0500

    friday

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -2,39 +2,60 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 
-/*
- * You will need to reuse the same paths many times over in the course of this sprint.
- * Consider using the `paths` object below to store frequently used file paths. This way,
- * if you move any files, you'll only need to change your code in one place! Feel free to
- * customize it in any way you wish.
- */
 
 exports.paths = {
+
   siteAssets: path.join(__dirname, '../web/public'),
   archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  list: path.join(__dirname, '../archives/sites.txt'),
+  index: path.join(__dirname, '../web/public/index.html')
+
 };
 
-// Used for stubbing paths for tests, do not modify
+
 exports.initialize = function(pathsObj){
   _.each(pathsObj, function(path, type) {
     exports.paths[type] = path;
   });
 };
 
-// The following function names are provided to you to suggest how you might
-// modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+exports.readListOfUrls = function(callback){
+	fs.readFile(exports.paths.list, function(err, data){
+		if( !err ){
+			var url = ('' + data).split('\n');
+		}
+		if (callback){
+			callback(url);
+		}			
+	});
 };
 
-exports.isUrlInList = function(){
+exports.isUrlInList = function(url, callback){
+	fs.readFile(exports.paths.list, function(err, data){
+		var result = false;
+		if ( !err ){
+			var urls = ('' + data).split('\n');
+			var test = _.indexOf(urls, url)
+			if (test > -1) {
+				result = true;
+			}
+		}
+		callback(result);
+	})
 };
 
-exports.addUrlToList = function(){
+exports.addUrlToList = function(newfile, callback){
+	fs.appendFile(exports.paths.list, newfile.url + '\n', function(err){
+  	if (err){
+  		console.log("err");
+  	}
+  	// callback();
+  });
 };
 
 exports.isUrlArchived = function(){
+
 };
 
 exports.downloadUrls = function(){
